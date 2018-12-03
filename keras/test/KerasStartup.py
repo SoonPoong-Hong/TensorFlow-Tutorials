25478573b68d725a7819dab8068e925b304b1768
rocklike <rocklike@gmail.com>
2018-12-04 00:29:51 +0900

    .

--- keras/test/KerasStartup.py
+++ keras/test/KerasStartup.py
@@ -6,8 +6,10 @@ Created on 2018. 12. 2.
 
 import tensorflow as tf
 from tensorflow.keras import layers
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
 
 print(tf.VERSION)
 print(tf.keras.__version__)
-
-print(tf.keras.__version__)
+print(tf.__version__)
