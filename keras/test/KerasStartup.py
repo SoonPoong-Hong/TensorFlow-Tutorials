659eac0d0a83ae09fb74de13db3786d8a14cb0c7
rocklike <rocklike@gmail.com>
2018-12-04 00:12:20 +0900

    .

---
+++ keras/test/KerasStartup.py
@@ -0,0 +1,13 @@
+'''
+Created on 2018. 12. 2.
+
+@author: hong
+'''
+
+import tensorflow as tf
+from tensorflow.keras import layers
+
+print(tf.VERSION)
+print(tf.keras.__version__)
+
+print(tf.keras.__version__)
