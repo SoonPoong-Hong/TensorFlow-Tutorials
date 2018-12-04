3b9de4a04207d4ecf3310e5d2a63de79263d335c
rocklike <rocklike@gmail.com>
2018-12-04 23:59:55 +0900

    .

--- keras/google/fashion_mnist.py
+++ keras/google/fashion_mnist.py
@@ -9,3 +9,33 @@ import matplotlib.pyplot as plt
 fashion_mnist = keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# print("== 길이")
+# print(train_images.shape[0])
+# print(len(train_labels))
+
+print("== plt 전")
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+# show()가 있어야, eclipse pydev에서 보여진다.
+# plt.show()
+print("== plt 땡.")
+
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+    
+plt.show()    
+    
+    
